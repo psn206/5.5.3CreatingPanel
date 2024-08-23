0e0e424516c9d19f59fbdd3f36c0c5c390e5d10c
Sergei Prutsakov <psn206@mail.ru>
2024-08-23 09:44:32 +0300

    Добавьте файлы проекта.

---
+++ CreatingPanel/Main.cs
@@ -0,0 +1,52 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CreatingPanel
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+           return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+
+            string TabName = "Панель плагинов";
+            application.CreateRibbonTab(TabName);
+            string utilsFolderPatch = @"C:\Program Files\RevitAPI\";
+            var panel = application.CreateRibbonPanel(TabName, "Приложения");
+
+            var buttonSystem = new PushButtonData("Система", "Смена ситстемы труб",
+                Path.Combine(utilsFolderPatch, "CreatingButtons.dll"),
+                "CreatingButtons.Main");
+
+            Uri imageButtonSystem = new Uri(@"C:\Program Files\RevitAPI\Image\Трубы.png", UriKind.Absolute);
+            BitmapImage mapImageSystem = new BitmapImage(imageButtonSystem);
+            buttonSystem.LargeImage = mapImageSystem;
+
+            var buttonType = new PushButtonData("Типы", "Смена типа стен",
+               Path.Combine(utilsFolderPatch, "ChangingWallTypes.dll"),
+               "ChangingWallTypes.Main");
+
+            Uri imageButtonType = new Uri(@"C:\Program Files\RevitAPI\Image\Стены.png", UriKind.Absolute);
+            BitmapImage mapImageType = new BitmapImage(imageButtonType);
+            buttonType.LargeImage = mapImageType;
+
+            panel.AddItem(buttonSystem);
+            panel.AddItem(buttonType);
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CreatingPanel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CreatingPanel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CreatingPanel")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ed606cd7-2a73-4268-9053-d336536a8de8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
